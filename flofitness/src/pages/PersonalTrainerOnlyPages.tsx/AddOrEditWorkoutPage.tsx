1407669cf1587d9eb830d31b2c7129ab6e07a7fd
Jim Berry <jimalexanderberry@gmail.com>
2021-03-08 23:22:25 +0000

    Adding workout number to page parser

--- flofitness/src/pages/PersonalTrainerOnlyPages.tsx/AddOrEditWorkoutPage.tsx
+++ flofitness/src/pages/PersonalTrainerOnlyPages.tsx/AddOrEditWorkoutPage.tsx
@@ -8,18 +8,23 @@ const AddOrEditWorkoutPage: React.SFC<AddOrEditWorkoutPageProps> = () => {
   const location = useLocation();
   const [workoutId, setWorkoutId] = useState('');
   const [workoutProgrammeId, setWorkoutProgrammeId] = useState('');
+  const [workoutNumber, setWorkoutNumber] = useState(0);
 
   useEffect(() => {
     const queryString = require('query-string');
     const parsed = queryString.parse(location.search);
     setWorkoutId(parsed.workoutId);
     setWorkoutProgrammeId(parsed.workoutProgrammeId);
+    if (parsed.workoutNumber) {
+      setWorkoutNumber(parsed.workoutNumber);
+    }
   }, [location]);
   return (
     <PageContainerStyles>
       <AddOrEditWorkoutContainer
         workoutId={workoutId}
         workoutProgrammeId={workoutProgrammeId}
+        workoutNumberIfAdding={workoutNumber}
       />
     </PageContainerStyles>
   );

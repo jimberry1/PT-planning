5c25d29e8db0cbc80de627fa7e6b46db83ab35ce
Jim Berry <jimalexanderberry@gmail.com>
2021-03-08 23:02:20 +0000

    Adding page route configuration

---
+++ flofitness/src/config/pageRoutes.tsx
@@ -0,0 +1,8 @@
+// PT routes
+export const ADD_OR_EDIT_WORKOUT = '/addOrEditWorkout';
+export const MANAGE_WORKOUT_PROGRAMME = '/manageWorkoutProgramme';
+export const CONTROL = '/control';
+
+// User routes
+export const WORKOUT = '/workout';
+export const EXERCISE_INFORMATION = '/exerciseInformation';
